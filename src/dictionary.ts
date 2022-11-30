b2a267e98afd87c6ad83e625c694a71c566d6ec3
oteroweb <oterolopez1990@gmail.com>
2022-11-30 16:42:47 -0400

    add dictionary for i18n

---
+++ src/dictionary.ts
@@ -0,0 +1,17 @@
+export const dictionary = {
+   register: {
+      headerText: 'Regístrate',
+      buttonText: 'Crear cuenta',
+      userInputText: 'Nombre de usuario',
+      emailInputText: 'Correo Electronico',
+      passInputText: 'Contraseña',
+   },
+   logout: {
+      buttonText: 'Cerrar Sesión',
+   },
+   home: {
+      labelUserText: "Nombre de usuario",
+      labelEmailText: "Correo Electroníco"
+   }
+
+}
