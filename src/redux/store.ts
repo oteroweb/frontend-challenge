5e6f3abc55ac454fe50b056dc1a51852dca4948e
oteroweb <oterolopez1990@gmail.com>
2022-11-30 16:49:11 -0400

    add simple config redux toolkit

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,40 +1,18 @@
-import {combineReducers, configureStore} from '@reduxjs/toolkit';
-// Or from '@reduxjs/toolkit/query/react'
-import {setupListeners} from '@reduxjs/toolkit/query';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const rootReducer = combineReducers([]);
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-const preloadedState = {};
-
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './auth'
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  preloadedState,
-  devTools:__DEV__,
-  // Adding the api middleware enables caching, invalidation, polling,
-  // and other useful features of `rtk-query`.
+  reducer: {
+    user: userReducer,
+  },
+  devTools: __DEV__,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
   // .concat(??.middleware),
 });
 
-const persistor = persistStore(store);
-
-// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
-// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
-setupListeners(store.dispatch);
-
-
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
-export {persistor};
+
