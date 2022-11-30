efcdf1eb3af95b6c0c1a3832170d5832b37a3e98
oteroweb <oterolopez1990@gmail.com>
2022-11-30 16:41:55 -0400

    delete unused components r-persist to reduxtoolkit

--- App.tsx
+++ App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import {Provider} from 'react-redux';
-import {persistor, store} from './src/redux/store';
-import {PersistGate} from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
 import Challenge from './src';
+import { theme } from "./src/theme";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor} loading={null}>
-        <AppContainer>
-          <Challenge/>
-        </AppContainer>
-      </PersistGate>
+      <Challenge />
     </Provider>
   );
 }
 
 const AppContainer = styled.View`
   flex: 1;
-  background-color: #ecf0f1;
+  background-color: $(theme.colors.white);
   align-items: center;
   justify-content: center;
 `;
--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,37 @@
 import React from 'react';
-import {Image, Text} from 'react-native';
+import { Image, Text, View, TouchableOpacity, TextInput, StyleSheet, SafeAreaView, Alert } from 'react-native';
 import styled from 'styled-components/native';
-import {useGetAvatar} from './hooks/useGetAvatar';
+import { useSelector } from 'react-redux'
 
+import Register from './screens/Register';
+import Home from './screens/Home';
+import { theme } from "./theme";
+
+import type { RootState } from './redux/store';
+import { isLoggedInSelector, register, setIsloggedIn } from './redux/auth'
+
+const RoundedContainer = styled.View<{ bgColor: string; padding: number }>`
+    flex: 1;
+    background-color: ${(props) => props.bgColor ?? 'transparent'};
+    padding: ${(props) => props.padding ?? 0}px;
+`;
 
 // Más instrucciones en https://yanatechnologies.notion.site/Frontend-Challenge-9427a4f79be54b5bb8c3ace2dd93a414
 const Challenge = () => {
-  const yanaAvatar = useGetAvatar('yana');
+  const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn)
   return (
-    <RoundedContainer bgColor={'white'} padding={12} >
-      <FlexRowVCenter>
-        <YanaAvatar source={yanaAvatar}/>
-        <TextInstructionsContainer>
-          <BigText>Instrucciones:</BigText>
-          <Text>Comienza editando el archivo <CodeText>src/index.tsx</CodeText></Text>
-        </TextInstructionsContainer>
-      </FlexRowVCenter>
+    <RoundedContainer bgColor={theme.white} padding={0} margin={0}>
+      {
+        !isLoggedIn
+          ? <Register />
+          : <Home />
+      }
     </RoundedContainer>
   );
 };
 
 export default Challenge;
 
-const BigText = styled.Text`
-  font-size: 24px;
-  font-weight: bold;
-  color: #000;
-`;
 const CodeText = styled.Text`
   font-size: 16px;
   font-weight: bold;
@@ -38,19 +43,9 @@ const YanaAvatar = styled(Image)`
   width: 100px;
   height: 100px;
 `;
-const FlexRowVCenter = styled.View`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`;
 const TextInstructionsContainer = styled.View`
   margin-left: 12px;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-`;
-const RoundedContainer = styled.View<{bgColor: string; padding: number}>`
-    background-color: ${(props) => props.bgColor ?? 'transparent'};
-    border-radius: 6px;
-    padding: ${(props) => props.padding ?? 0}px;
 `;
