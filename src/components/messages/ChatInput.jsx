1c5d7901a89ebc5e1b831aa59005f4091244503d
oteroweb <oterolopez1990@gmail.com>
2022-12-01 03:41:24 -0400

    function chat

--- src/components/messages/ChatInput.jsx
+++ src/components/messages/ChatInput.jsx
@@ -1,12 +1,6 @@
 import React, { useState } from "react";
-import {
-	View,
-	Text,
-	StyleSheet,
-	TextInput,
-	Platform,
-	TouchableOpacity,
-} from "react-native";
+import { useDispatch } from 'react-redux'
+import { sendMessage } from '../../redux/auth'
 
 import styled from 'styled-components/native';
 
@@ -46,8 +40,26 @@ const StyledTextInput = styled.TextInput`
 	height: 56px;
 	align-self: center;
 `
+const StyledTouchableOpacity = styled.TouchableOpacity`
+	background-color: ${theme.colors.primary};
+		border-radius: 50;
+		height: 50px;
+		width: 50px;
+		align-items: center;
+		justify-content: center;
+`
+
 const ChatInput = () => {
 	const [message, setMessage] = useState("");
+	const dispatch = useDispatch()
+
+
+	const handleSendMessage = () => {
+		if (message) {
+			dispatch(sendMessage(message));
+			setMessage(null);
+		}
+	}
 	return (
 		<StyledContainer>
 			<StyledInnerContainer >
@@ -59,123 +71,16 @@ const ChatInput = () => {
 						onChangeText={(text) => setMessage(text)}
 					/>
 				</StyledInputBar>
-				<TouchableOpacity style={styles.sendButton}>
+				<StyledTouchableOpacity onPress={handleSendMessage} >
 					<Icon
 						name="send"
 						size={23}
 						color={theme.colors.white}
 					/>
-				</TouchableOpacity>
+				</StyledTouchableOpacity>
 			</StyledInnerContainer>
 		</StyledContainer>
 	);
 };
-
-const styles = StyleSheet.create({
-	container: {
-		justifyContent: "center",
-		backgroundColor: theme.colors.white,
-	},
-	replyContainer: {
-		paddingHorizontal: 10,
-		marginHorizontal: 10,
-		justifyContent: "center",
-		alignItems: "flex-start",
-	},
-	title: {
-		marginTop: 5,
-		fontWeight: "bold",
-	},
-	closeReply: {
-		position: "absolute",
-		right: 10,
-		top: 5,
-	},
-	reply: {
-		marginTop: 5,
-	},
-	innerContainer: {
-		paddingHorizontal: 10,
-		marginHorizontal: 10,
-		justifyContent: "space-between",
-		alignItems: "center",
-		flexDirection: "row",
-		paddingVertical: 10,
-	},
-	inputAndMicrophone: {
-		flexDirection: "row",
-		backgroundColor: theme.colors.inputBackground,
-		flex: 3,
-		marginRight: 10,
-		paddingVertical: Platform.OS === "ios" ? 10 : 0,
-		borderRadius: 30,
-		alignItems: "center",
-		justifyContent: "space-between",
-	},
-	input: {
-		backgroundColor: "transparent",
-		paddingLeft: 20,
-		color: theme.colors.inputText,
-		flex: 3,
-		fontSize: 15,
-		height: 50,
-		alignSelf: "center",
-	},
-	rightIconButtonStyle: {
-		justifyContent: "center",
-		alignItems: "center",
-		paddingRight: 15,
-		paddingLeft: 10,
-		borderLeftWidth: 1,
-		borderLeftColor: "#fff",
-	},
-	swipeToCancelView: {
-		flexDirection: "row",
-		alignItems: "center",
-		marginRight: 30,
-	},
-	swipeText: {
-		color: theme.colors.description,
-		fontSize: 15,
-	},
-	emoticonButton: {
-		justifyContent: "center",
-		alignItems: "center",
-		paddingLeft: 10,
-	},
-	recordingActive: {
-		flexDirection: "row",
-		alignItems: "center",
-		justifyContent: "space-between",
-		paddingLeft: 10,
-	},
-	recordingTime: {
-		color: theme.colors.description,
-		fontSize: 20,
-		marginLeft: 5,
-	},
-	microphoneAndLock: {
-		alignItems: "center",
-		justifyContent: "flex-end",
-	},
-	lockView: {
-		backgroundColor: "#eee",
-		width: 60,
-		alignItems: "center",
-		borderTopLeftRadius: 30,
-		borderTopRightRadius: 30,
-		height: 130,
-		paddingTop: 20,
-	},
-	sendButton: {
-		backgroundColor: theme.colors.primary,
-		borderRadius: 50,
-		height: 50,
-		width: 50,
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
-
 
 export default ChatInput;
